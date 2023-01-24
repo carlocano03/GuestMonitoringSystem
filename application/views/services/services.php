9f6e46fd8abfb9d7a9f09117471288f832f5515d
Carlo Cano <carlocano03@gmail.com>
2023-01-24 20:34:30 +0800

    add services

---
+++ application/views/services/services.php
@@ -0,0 +1,72 @@
+<style>
+    body {
+        background-image: url("<?= base_url('assets/img/bg-img.png') ?>");
+        background-size: cover;
+        background-attachment: fixed;
+    }
+</style>
+
+<body>
+    <main>
+        <div class="container d-flex justify-content-center min-vh-100">
+            <div class="container mt-3">
+                <div class="text-end mb-5 pb-3">
+                    <button class="btn btn-warning fw-bold">GO BACK</button>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="inflatables">
+                            <img class="img-icon" src="<?= base_url('assets/img/logo/inflatables.png'); ?>" alt="Inflatables">
+                            <ul>
+                                <li>The inflatables are only suitable for children <span class="text-white fw-bold">12 y/o and below only.</span></li>
+                                <li>All children within the premises <span class="text-white fw-bold">must be accompanied by a parent/guardian</span>
+                                    at all times who is eighteen (18) years old to (60) years old only.
+                                </li>
+                                <li>A <span class="text-white fw-bold">maximum of two (2) kids per parent/guardian</span> only is allowed.</li>
+                                <li>We strictly implement <span class="text-white fw-bold">NO Guardian No Entry</span> on our facilities
+                                    for children 12 y/o and below.
+                                </li>
+                                <li>Wearing <span class="text-white fw-bold">grip socks</span> while playing or staying inside our facilities is required.</li>
+                                <li>Only the same parent or guardian whose signature appears on the registration form and who has taken the picture together with
+                                    the child by our camera upon entry can check out the child.
+                                </li>
+                                <li>The guardian is responsible for the child's behavior and compliance with Jacks's Adventure rules and regulations at all times.</li>
+                                <li><span class="text-white fw-bold">Leaving</span> the children inside the inflatable alone to go to Jack's Adventure Park is not allowed.</li>
+                                <li><span class="text-white fw-bold">Running or climbing inside the play area</span> must be avoided at all times,
+                                    as this may result in injury.
+                                </li>
+                                <li>Should you need to temporarily leave the area, please observe Jack's Adventure rules for re-entry.</li>
+                                <li>All customers are required to observe <span class="text-white"><u>Jack's Adventures General Guidelines</u></span> at all times.</li>
+                            </ul>
+                            <div class="btn-div">
+                                <button class="btn-register">CLICK HERE TO REGISTER <img src="<?= base_url('assets/img/arrow.gif'); ?>" alt="" width="80"></button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="park">
+                            <img class="img-icon" src="<?= base_url('assets/img/logo/park.png'); ?>" alt="Park">
+                            <ul>
+                                <li>All Jack's Adventure play parks are for age's <span class="text-white fw-bold">13 y/o and up only.</span></li>
+                                <li>Customers must observe all <span class="text-white fw-bold">safety rules of the modules at all times to avoid injuries.</span></li>
+                                <li>You will be asked to leave the facility if you do not observe the <span class="text-white fw-bold">safety rules.</span></li>
+                                <li>Read and understand all rules and gameplay of all play parks before proceeding.</li>
+                                <li>Please use the facility in compliance with safety rules, playtime, and height and weight requirement by content.</li>
+                                <li>Please <span class="text-white fw-bold">stretch and warm up your body and wear grip socks</span> before using the facility.</li>
+                                <li>All play parks are <span class="text-white fw-bold">not advisable for persons over 60 y/o, person with disablities, sick,
+                                    with contagious diseases, or injuries, and persons with special needs.</span>
+                                </li>
+                                <li>Jack's Adventure may require <span class="text-white fw-bold">medical clearance</span> in the exercise of prudence and due diligence.</li>
+                                <li>All customers are required to observe <span class="text-white"><u>Jack's Adventures General Guidelines</u></span> at all times.</li>
+                            </ul>
+                            <br><br>
+                            <div class="btn-div mt-3">
+                                <button class="btn-register">CLICK HERE TO REGISTER <img src="<?= base_url('assets/img/arrow.gif'); ?>" alt="" width="80"></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
